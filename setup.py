6a9d1100de2df893b35b114522d637e4bd527927
Ondrej Platek <ondrej.platek@seznam.cz>
2021-04-26 21:27:34 +0200

    add url, download_url, classifiers

--- setup.py
+++ setup.py
@@ -13,7 +13,20 @@ setup(
     author='Ondrej Platek',
     author_email='ondrej.platek@seznam.cz',
     license='Apache-2.0 License',
+    url='https://github.com/oplatek/exp-notifier',
+    donwload_url='https://github.com/oplatek/exp-notifier/archive/refs/tags/0.0.1.tar.gz',
     packages=find_packages(),
     scripts=['notifier/bin/ntf'],
     install_requires=install_requires,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',      # Define that your audience are developers
+        'Topic :: System :: Logging',
+        'Topic :: System :: Networking :: Monitoring',
+        'License :: OSI Approved :: Apache Software License'
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
 )
